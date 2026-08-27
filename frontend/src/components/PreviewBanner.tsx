803fcef7e064304ebf8cf2c21acb3cad7fa10e60
Mannie <mannietech817@gmail.com>
2026-08-27 03:26:49 +0100

    feat(banner): add banner title

--- frontend/src/components/PreviewBanner.tsx
+++ frontend/src/components/PreviewBanner.tsx
@@ -1,2 +1,2 @@
 import { Link } from 'react-router-dom';
-export const PreviewBanner = () => { return (<div className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white flex justify-between items-center px-4 py-3 cursor-pointer"><div></div></div>); };
+export const PreviewBanner = () => { return (<div className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white flex justify-between items-center px-4 py-3 cursor-pointer"><div><p className="text-[12px] uppercase tracking-widest font-bold mb-1">Preview of Spotify</p></div></div>); };
