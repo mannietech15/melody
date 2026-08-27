94f4aaf77d2e55661670eea67eac21462442d788
Mannie <mannietech817@gmail.com>
2026-08-27 03:26:48 +0100

    style(banner): add layout and padding to banner

--- frontend/src/components/PreviewBanner.tsx
+++ frontend/src/components/PreviewBanner.tsx
@@ -1,1 +1,2 @@
-export const PreviewBanner = () => { return (<div className="bg-gradient-to-r from-[#af2896] to-[#509bf5]"></div>); };
+import { Link } from 'react-router-dom';
+export const PreviewBanner = () => { return (<div className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white flex justify-between items-center px-4 py-3 cursor-pointer"></div>); };
