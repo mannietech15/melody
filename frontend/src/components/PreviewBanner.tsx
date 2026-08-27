902eaa10ca05b95af649cb1d072c07548cd1cfed
Mannie <mannietech817@gmail.com>
2026-08-27 03:26:49 +0100

    feat(banner): add text container

--- frontend/src/components/PreviewBanner.tsx
+++ frontend/src/components/PreviewBanner.tsx
@@ -1,2 +1,2 @@
 import { Link } from 'react-router-dom';
-export const PreviewBanner = () => { return (<div className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white flex justify-between items-center px-4 py-3 cursor-pointer"></div>); };
+export const PreviewBanner = () => { return (<div className="bg-gradient-to-r from-[#af2896] to-[#509bf5] text-white flex justify-between items-center px-4 py-3 cursor-pointer"><div></div></div>); };
